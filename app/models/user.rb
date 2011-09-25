86e11cb95f1604e06f77774c15d31ef07adfb079
Tal Auslander <talaus@gmail.com>
2011-09-25 19:34:20 +0300

    Done with sign in/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,6 +46,11 @@ class User < ActiveRecord::Base
     # if statement returns nil when condition is false
   end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
   private
 
     def encrypt_password
